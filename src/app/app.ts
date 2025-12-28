b408bdd86217a1ec20ff2ce3c9cf4e75eec2b0e1
kronhardtluis <kronhardtluis@gmail.com>
2025-12-28 23:42:30 +0100

    Erstellung des Projekts Portfolio

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./header/header";
+import { Mainpage } from "./mainpage/mainpage";
+import { Footer } from "./footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Mainpage, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
