03e1fd56eaa940b858f9e8ac688700292bd7847f
kronhardtluis <kronhardtluis@gmail.com>
2025-12-29 17:51:31 +0100

    Hauptseitenkomponenten erstellt; header und head angepasst; Variablen und Mixin hinzugefügt

--- src/app/mainpage/mainpage.ts
+++ src/app/mainpage/mainpage.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { Head } from "./head/head";
+import { AboutMe } from "./about-me/about-me";
+import { Skills } from "./skills/skills";
+import { Projects } from "./projects/projects";
+import { Comments } from "./comments/comments";
+import { Contact } from "./contact/contact";
 
 @Component({
   selector: 'app-mainpage',
-  imports: [],
+  imports: [Head, AboutMe, Skills, Projects, Comments, Contact],
   templateUrl: './mainpage.html',
   styleUrl: './mainpage.scss',
 })
