80bf961f75c63cd1f437a3785e4ec8b5af9b4883
kronhardtluis <kronhardtluis@gmail.com>
2026-05-08 20:38:38 +0200

    formValidation und dazugehörige Error-Meldungen gesetzt

--- src/app/mainpage/contact/contact.ts
+++ src/app/mainpage/contact/contact.ts
@@ -1,22 +1,41 @@
 import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-contact',
-  imports: [TranslatePipe, RouterLink],
+  imports: [TranslatePipe, RouterLink, ReactiveFormsModule],
   templateUrl: './contact.html',
   styleUrl: './contact.scss',
 })
 export class Contact {
-  checkbox:boolean = false;
+  name: FormControl = new FormControl('', {
+    validators: [Validators.required, Validators.minLength(2), Validators.pattern(/^[\p{L}\p{Zs}\-]+$/u)]
+  });
+
+  email: FormControl = new FormControl('', {
+    validators: [Validators.required, Validators.email]
+  });
+
+  message: FormControl = new FormControl('', {
+    validators: [Validators.required, Validators.minLength(3)]
+  });
+
+  checkbox: FormControl = new FormControl(false, Validators.requiredTrue);
 
   toggleCheckbox():void {
-    this.checkbox = !this.checkbox;
+    this.checkbox.setValue(!this.checkbox.value);
+    this.checkbox.markAsDirty();
   }
 
   scrollUp():void {
     window.scroll({ top: 0, behavior: 'smooth' })
+  }
+
+  updateForm():void {
+    this.name.setValue("");
+    this.email.setValue("");
+    this.message.setValue("");
   }
 }
