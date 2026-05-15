5b4d20ee7a25c7471e94f86e79150f0d94b44b7c
kronhardtluis <kronhardtluis@gmail.com>
2026-05-15 16:59:38 +0200

    Github-Links eingefügt

--- src/app/mainpage/projects/projects.ts
+++ src/app/mainpage/projects/projects.ts
@@ -5,7 +5,8 @@ interface projectFace {
   name: string;
   description: string;
   technologys: string;
-  img: string
+  img: string;
+  gitLink: string;
 }
 
 @Component({
@@ -21,13 +22,15 @@ export class Projects {
       name: "El Pollo Loco (in progress)",
       description: "Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.",
       technologys: "Angular | TypeScript | HTML | CSS | Firebase",
-      img: '/assets/img/PolloLoco.png'
+      img: '/assets/img/PolloLoco.png',
+      gitLink: 'https://github.com/kronhardtluis/El-Pollo-Loco'
     },
     {
-      name: "Join (in progress)",
+      name: "Join",
       description: "Task manager inspired by Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.",
       technologys: "Angular | TypeScript | HTML | CSS | Firebase",
-      img: '/assets/img/join-laptop.png'
+      img: '/assets/img/join-laptop.png',
+      gitLink: 'https://github.com/kronhardtluis/join'
     }
   ]
 }
